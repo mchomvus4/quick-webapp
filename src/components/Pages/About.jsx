d1b825e91aa349e5a61d31112f0fa6eac6bbb468
samwelnm4@gmail.com <samwelnm4@gmail.com>
2021-07-22 23:33:09 +0300

    about.js updated

--- src/components/Pages/About.jsx
+++ src/components/Pages/About.jsx
@@ -1,10 +1,26 @@
 import React from 'react'
-
+import carbon from '../../images/banner.jpg'
 const About = () => {
   return (
     <div className="container">
-      Welcome to about us page
+    <section className="about-us">
+      <p className="quickreg">
+        
+      </p>
+    </section>
+    <div className="main-content">
+      <div>
+        <img src={carbon} alt="" />
+      </div>
+      <div>
+        <p>
+          Lorem ipsum dolor sit amet consectetur adipisicing elit. Earum aspernatur cum nemo at dolores debitis quasi fuga molestiae tenetur nostrum ex ab ipsam maxime, est vitae. Enim incidunt, debitis quam optio ab repellat amet rerum, nostrum a beatae dolorum fugit necessitatibus. Ducimus qui autem nostrum labore aut facere maxime, necessitatibus dolore delectus, quibusdam, dolor sunt! Quaerat, ullam doloribus! Eaque doloremque veritatis maxime repellendus ducimus perspiciatis aperiam incidunt unde porro tempora asperiores perferendis, tempore iure autem molestias vel, quos animi possimus libero facilis laudantium ratione dicta dolorum voluptatum. Libero totam assumenda, culpa sapiente doloribus minima quasi perspiciatis eaque quis non, unde illo maiores eos. Consequatur, cupiditate corporis. Maxime vel expedita quae possimus modi odio, ducimus necessitatibus sint quod. Assumenda eveniet iure itaque, id veniam sit, architecto blanditiis porro non et laudantium animi minima! Iusto dolore rerum in excepturi. Fugit esse totam, reiciendis eum asperiores distinctio quis labore illo, qui tenetur repellat velit tempora veniam ea exercitationem illum tempore doloribus. Ut nihil velit, impedit nostrum eius eaque doloribus eum suscipit quia ipsam quaerat voluptates, inventore asperiores fuga magnam dolorum reprehenderit, debitis praesentium reiciendis incidunt iure deleniti ea? Quo minima quod dolorum error aliquid, voluptatum eos nihil excepturi nostrum. Quia iure voluptates incidunt recusandae? Vero ut quis sunt voluptatem, ipsam odit veritatis iste quo facilis enim necessitatibus impedit, a beatae veniam laboriosam adipisci harum minima laborum quibusdam vitae fugit repellendus voluptates voluptas distinctio. Sint ratione enim, perferendis, accusamus impedit eaque maxime dolore incidunt totam culpa soluta? Cum aperiam dicta reprehenderit modi consectetur harum perspiciatis, quos eaque dolorem aut natus hic, ipsa ad facilis maiores voluptates numquam tenetur laborum dolorum. Ad aliquam eveniet dolore, inventore porro quia quidem maiores, repellat reiciendis eaque sunt? Consequuntur ipsam reiciendis sunt provident laboriosam inventore libero enim nam? Molestiae suscipit labore possimus culpa, laborum, modi unde id tempore obcaecati impedit non, nostrum rem dolorum libero explicabo magnam adipisci. Consectetur ducimus culpa sint debitis ab deleniti quisquam inventore molestias, dolorum aperiam. Autem, at accusamus similique magni nulla enim recusandae voluptate laudantium voluptates consectetur et assumenda placeat, delectus incidunt minus repellat deleniti dolorem ad atque dolore tempore? Facilis, repudiandae, tempore explicabo culpa cumque quod quia dolor quaerat minima maxime dolorum sed accusamus dolores asperiores? Labore minima quis consectetur molestias itaque. Quia sapiente aliquam nulla. Nihil dolor accusamus minus facilis, dolorum, quam eaque veritatis maxime quis culpa, odio facere blanditiis officia pariatur quos perferendis necessitatibus vero exercitationem fugiat rerum laudantium cum ipsum.
+        </p>
+      </div>
     </div>
+   
+    </div>
+  
   )
 }
 
